798d1151ab79f1366980ba8db183601bc0b3e284
Hedyfiiii <huangxiaohuihedy@gmail.com>
2025-11-23 23:00:52 -0800

    create ci/cd

---
+++ RecommendationSystemdemo/src/main/java/com/example/recommendationsystem/RecommendationSystemdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.recommendationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationSystemdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationSystemdemoApplication.class, args);
+    }
+
+}
